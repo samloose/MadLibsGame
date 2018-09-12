5d2d22ab89d194e2e0e76a4834a1dbf82a1f30e4
sam.loose <sloose@jaguar.csuchico.edu>
2018-09-12 16:59:49 -0700

    Program complete

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,37 @@
-//Authors: 
+//Authors: Sam Loose 
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main()
 {
+string a;
+string b;
+string c; 
+string d;
+string e;
+string f;
+int g;
+string h;
+    cout<<"Enter a Verb"<<endl;
+    cin>>a;
+    cout<<"Enter an Adjective"<<endl;
+    cin>>b;
+    cout<<"Enter a nown(plural)"<<endl;
+    cin>>c;
+    cout<<"Enter an Adjective"<<endl;
+    cin>>d;
+    cout<<"Enter a Verb ending in 'ING'"<<endl;
+    cin>>e;
+    cout<<"Enter a Verb"<<endl;
+    cin>>f;
+    cout<<"Enter a Number"<<endl;
+    cin>>g;
+    cout<<"Enter an Adjective"<<endl;
+    cin>>h;
 
+        cout<<"Come "<<a<<" at Walmart, where you'll receive "<<b<<" discounts on all your favorite brand name "<<c<<"! Our "<<d<<" and "<<e<<" associates are there to "<<f<<" you "<<g<<" hours a day! So come down to Walmart where the "<<h<<" come first!"<<endl;
+            
+            
   return 0;
 }
